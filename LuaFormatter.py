7459c87449dd33f9b4bfbd4c28eafaeb5bcb9dac
Richard <1151532084@qq.com>
2022-06-11 11:48:06 +0800

    更新到作者的1.3.8版本

--- LuaFormatter.py
+++ LuaFormatter.py
@@ -1,87 +1,33 @@
 import os
+import sublime, sublime_plugin, sys
 import subprocess
-import sys
-
-import sublime
-import sublime_plugin
-
-
-def get_st_project_path():
-    """Get the active Sublime Text project path.
-    Original: https://gist.github.com/astronaughts/9678368
-    :rtype: object
-    :return: The active Sublime Text project path.
-    """
-    window = sublime.active_window()
-    folders = window.folders()
-    if len(folders) == 1:
-        return folders[0]
-    else:
-        active_view = window.active_view()
-        if active_view:
-            active_file_name = active_view.file_name()
-        else:
-            active_file_name = None
-        if not active_file_name:
-            return folders[0] if len(folders) else os.path.expanduser("~")
-        for folder in folders:
-            if active_file_name.startswith(folder):
-                return folder
-        return os.path.dirname(active_file_name)
-
-
-def get_file_abs_dir(filepath):
-    return os.path.abspath(os.path.dirname(filepath))
-
-
-def find_config(start_dir, alt_dirs=None):
-    config_file = ".lua-format"
-    target = os.path.join(start_dir, config_file)
-    if os.path.exists(target):
-        return target
-    return None
 
 
 class LuaFormatCommand(sublime_plugin.TextCommand):
-    def try_find_config(self, view):
-        project_file_dir = get_st_project_path()
-
-        # 1. Attempt to automatically resolve:
-        resolved_config = find_config(project_file_dir)
-        if resolved_config and os.path.exists(resolved_config):
-            return resolved_config
-
-        return None
-
     def run(self, edit, error=True, save=True):
         # run lua-format
         package_path = os.path.split(os.path.dirname(__file__))[1]
-        executable_path = os.path.join(
-            sublime.packages_path(), package_path, "bin", sys.platform, "lua-format"
-        )
+        executable_path = os.path.join(sublime.packages_path(), package_path, "bin", sys.platform, "lua-format")
 
         contents = self.view.substr(sublime.Region(0, self.view.size()))
 
         cmd = [executable_path]
-        config_file_settings = sublime.load_settings(
-            "LuaFormatter.sublime-settings"
-        ).get("config_file", "")
-        config_file_project = self.try_find_config(self.view)
-        config_file = config_file_settings
-
-        if config_file_project:
-            config_file = config_file_project
-
-        if config_file != "":
+        configFile = sublime.load_settings("LuaFormatter.sublime-settings").get("config_file", "")
+        if configFile != "":
             cmd.append("-c")
-            cmd.append(config_file)
+            cmd.append(configFile)
 
-        process = subprocess.Popen(
-            cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE
-        )
+        startupinfo = None
+        IS_WIN32 = "win32" in str(sys.platform).lower()
+        if IS_WIN32:
+            startupinfo = subprocess.STARTUPINFO()
+            startupinfo.dwFlags = subprocess.CREATE_NEW_CONSOLE | subprocess.STARTF_USESHOWWINDOW
+            startupinfo.wShowWindow = subprocess.SW_HIDE
+
+        process = subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, startupinfo=startupinfo)
         process.stdin.write(str.encode(contents))
         process.stdin.close()
-        output = bytes.decode(process.stdout.read())
+        output = bytes.decode(process.stdout.read()).replace("\r\n", "\n")
         error = bytes.decode(process.stderr.read())
         if error == "":
             self.view.replace(edit, sublime.Region(0, self.view.size()), output)
